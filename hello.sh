4ab7dbfca992b9815676c84b227021f0c876d89a
shibu <shibu.rkmv@gmail.com>
2025-01-09 13:18:33 +0530

    "added bin bash"

--- hello.sh
+++ hello.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-OPENAI_API_KEY="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"
+OPENAI_API_KEY="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"
 PROMPT="Tell me a joke"
 
 curl "https://api.openai.com/v1/chat/completions" \
