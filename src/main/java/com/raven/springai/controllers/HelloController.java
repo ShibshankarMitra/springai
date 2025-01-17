fb640ea1f6800dc949accd471404182ae5631c47
shibu <shibu.rkmv@gmail.com>
2025-01-17 18:48:17 +0530

    "added bin bash"

--- src/main/java/com/raven/springai/controllers/HelloController.java
+++ src/main/java/com/raven/springai/controllers/HelloController.java
@@ -3,11 +3,9 @@ package com.raven.springai.controllers;
 import com.raven.springai.models.GptPrompt;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -19,7 +17,15 @@ public class HelloController {
     @PostMapping("/prompt")
     public ResponseEntity<String> generateAnswer(
             @RequestBody GptPrompt gptPrompt){
-        String response = gptChatClient.prompt(gptPrompt.getPrompt()).call().content();
+        String response = gptChatClient.prompt(new Prompt( gptPrompt.getPrompt())).call().content();
        return ResponseEntity.ok(response);
     }
+
+    @GetMapping("/youtube/popular")
+    public ResponseEntity<String> generatePopularYoutuber(
+            @RequestParam(value = "genre") String genre){
+        String response = gptChatClient.prompt(new Prompt("Any")).call().content();
+        return ResponseEntity.ok(response);
+    }
+
 }
